7563242d8f1847d9cb654cfdb3b3bc521801018d
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-06-13 10:03:30 -0300

    Tópico 4, Configurando o Pool de Conexões

--- src/main/java/br/com/rodolfo/loja/JpaConfigurator.java
+++ src/main/java/br/com/rodolfo/loja/JpaConfigurator.java
@@ -11,15 +11,29 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class JpaConfigurator {
     
-
-    // @Bean
+	//O SPRING BOOT realiza a configuração no arquivo properties.
+	//O atributo 'destroyMethod' define o método 'close' do Pool que o Spring chama Quando o TOMCAT é desligado, assim garantimos que todas as conexões serão fechadas corretamente
+    // @Bean(destroyMethod="close")
 	// public DataSource getDataSource() {
-	//     DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	//	   //O DriverManagerDataSource cria uma nova conexão a cada uso o que leva a não ser uma boa solução
+	//     //DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	//	   //Abrir conexão com pool de conexões utilizando o C3P0
+	//	   ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
 	//     dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 	//     dataSource.setUrl("jdbc:mysql://127.0.0.1/lista");
 	//     dataSource.setUsername("root");
 	//     dataSource.setPassword("");
+
+	//	   //Criar conexões deixando-as disponíveis
+	//	   dataSource.setMinPoolSize(5);
+	//	   //Limitando o número de conexões
+	//	   dataSource.setMaxPoolSize(9);
+	//	   //O C3P0 solicita a configuração de número de threads auxiliares para distribuir o processamento porque o JDBC possui alguns processamentos lentos e fazendo isso ganha-se performance
+	//	   dataSource.setNumHelperThreads(5)
+
+	//	   //Para evitar que o Pool conteha conexões que podem quebrar com o tempo (ex.: o banco de dados cai e retorna quebrando as conexões do pool) existe o método 'setIdleConnectionTestPeriod' que elimina as conexões que ficam ociosas por muito tempo no pool elimando o risco de escolher uma conexão quebrada.
+	//	   dataSource.setIdleConnectionTestPeriod(1); //a cada um segundo testamos as conexões ociosas
 
 	//     return dataSource;
 	// }
